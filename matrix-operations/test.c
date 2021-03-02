88d77d60d7cd46b8333009adce9d86ecaa97913c
Manuele Rusci <manuele.rusci@unibo.it>
2021-03-02 19:45:36 +0100

    added empy assignment files

---
+++ matrix-operations/test.c
@@ -0,0 +1,18 @@
+/* 
+ * Copyright (C) 2020 University of Bologna
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ *
+ */
+
+#include "pmsis.h"
+
+
+// write your functions here
+
+int main()
+{
+	// write your main code here
+}
